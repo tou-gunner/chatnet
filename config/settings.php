3fb99af0125d5efad8f3e4d1360cfafddaef0cb8
keevue <keevue19@gmail.com>
2022-06-25 10:57:10 +0700

    update 25/6/2022

--- config/settings.php
+++ config/settings.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_USER', 'alochat');
-define('DB_PASSWORD', 'mmongKp123m');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'alochat');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'cn_');
